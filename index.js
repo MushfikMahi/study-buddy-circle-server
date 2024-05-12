950fee5025a60d1b90183d7accbc82617d786693
MushfikMahi <salehinmahi2004@gmail.com>
2024-05-13 01:09:15 +0600

    token cleared from the client

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('dotenv').config()
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser')
 const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
@@ -37,13 +38,26 @@ const client = new MongoClient(uri, {
     const submittedCollection = client.db('assignmentsDB').collection('submitted')
 
 
-    // jwt 
+    // jwt genetrate
     app.post('/jwt', async(req, res)=>{
       const user = req.body
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn:'365d'})
-      res.send({token})
+      res.cookie('token',token,{
+        httpOnly: true,
+        secure: process.env.NODE_ENV==='production',
+        sameSite: process.env.NODE_ENV==='production'?'none':'strict'
+      }).send({success: true})
     })
 
+    // clear token 
+    app.get('/logout', (req, res)=>{
+      res.clearCookie('token', {
+        httpOnly:true,
+        secure: process.env.NODE_ENV==='production',
+        sameSite: process.env.NODE_ENV==='production'?'none':'strict',
+        maxAge: 0,
+      }).send({success: true})
+    })
 
     app.post('/assignments', async(req, res)=>{
         const assignments = req.body;
